a34b1b555513aca9f2fcab6a308a896934b7fb8e
MohomedShalik <mohomedshalik2@gmail.com>
2020-07-24 11:13:55 +0530

    certainity grid created

---
+++ local_map/src/LocalMapper.cpp
@@ -0,0 +1,122 @@
+#include <nav_msgs/GridCells.h>
+#include <math.h>
+
+#include <local_map/LocalMapper.h>
+
+
+
+#define WIDTH 100
+#define HEIGHT 100
+
+LocalMapper ::LocalMapper () : mTf2Buffer(), mTf2Listener(mTf2Buffer)
+{
+	// Create the local costmap
+	mLocalMap = new costmap_2d::Costmap2DROS("local_map", mTf2Buffer);
+	
+	
+	// Publish / subscribe to ROS topics
+	ros::NodeHandle robotNode;
+	robotNode.param("robot_frame", mRobotFrame, std::string("base_link"));
+	robotNode.param("odometry_frame", mOdometryFrame, std::string("odom"));
+
+	mRobotFrame = mTfListener.resolve(mRobotFrame);
+	mOdometryFrame = mTfListener.resolve(mOdometryFrame);
+
+
+}
+
+LocalMapper ::~LocalMapper ()
+{
+		ROS_INFO("DESTROYING MAPPER");
+} 
+
+
+void LocalMapper::get_certainity_grid()
+{
+	int p = 0;
+	ROS_WARN("mapping ceartinity grid");
+	for(int i = 0 ;i < WIDTH ; ++i){
+		for(int j = 0 ; j < HEIGHT ; ++j){
+			int l = static_cast<int>(mCostmap->getCost(i , j));
+			double d_i_j = sqrt((49 - i)*(49 - i) + (49 - j)*(49 - j));
+			if (int(d_i_j) < 6){
+				++p;
+				continue;
+			}
+			if (l > 0)
+				_certainity_grid[i][j] += 1;
+				continue;
+			if (l = 0){
+				if (_certainity_grid[i][j] == 0)
+					continue;
+				else if (_certainity_grid[i][j] > 0)
+				{
+					_certainity_grid[i][j] -= 1;
+				}
+				
+			}
+		}
+
+	}
+
+	ROS_WARN("end mapping ceartinity grid");
+	ROS_WARN("%d " , p);
+}
+
+void LocalMapper::get_vector_magnitude()
+{
+
+
+	for(int i = 0 ;i < WIDTH ; ++i){
+		for(int j = 0 ; j < HEIGHT ; ++j){
+
+			if (int(sqrt((49 - i)*(49 - i) + (49 - j)*(49 - j))) < 6){
+				
+				continue;
+			}
+			double d_i_j =  sqrt((49 - i)*(49 - i) + (49 - j)*(49 - j));
+			_m_grid[i][j] = (_certainity_grid[i][j] * _certainity_grid[i][j])*(_a - _b * (d_i_j));
+
+
+		}
+	}
+
+}
+
+void LocalMapper::build_polar_histogram()
+{
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+void LocalMapper ::buildPolarHistogram()
+{
+	// 1. Get a copy of the costmap to work on.
+	mCostmap = mLocalMap->getCostmap();
+	mLocalMap->updateMap();
+	boost::unique_lock<costmap_2d::Costmap2D::mutex_t> lock(*(mCostmap->getMutex()));
+	get_vector_magnitude();
+	
+}
+
+
+
+double diff(double v1, double v2)
+{
+	if(v1 > v2)
+		return v1 - v2;
+	else
+		return v2 - v1;
+}
+
+
